fb97aca462c845ab34b54b991cc6cf30a96c2146
khaled ataa <khaled.mohamed.ataa2003@gmail.com>
2024-09-15 14:51:45 +0300

    add custom app bar to edit note view

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({super.key, required this.title, required this.icon});
+  final String title;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
-          'Notes', // ignore: prefer_const_constructors
-          style: TextStyle(
+          title, // ignore: prefer_const_constructors
+          style: const TextStyle(
             fontSize: 28,
             color: Colors.white,
             decoration: TextDecoration.none,
           ),
         ),
-        Spacer(),
-        CustomSearchIcon(),
+        CustomIcon(
+          icon: icon,
+        ),
       ],
     );
   }
--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          CustomAppBar(
+            title: 'Edit Note',
+            icon: Icons.check,
+          ),
+          CustomTextField(title: 'title'),
+          CustomTextField(
+            title: 'title',
+            maxLines: 5,
+          ),
+        ],
+      ),
+    );
   }
 }
