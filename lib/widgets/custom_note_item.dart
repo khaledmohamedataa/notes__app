f87d7c387f672ce98d9028b79cd842513dad4957
khaled ataa <khaled.mohamed.ataa2003@gmail.com>
2024-09-18 06:41:54 +0300

    display data

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -26,15 +27,15 @@ class NoteItem extends StatelessWidget {
             left: 16,
           ),
           decoration: BoxDecoration(
-            color: const Color(0xffFFCC80),
+            color: Color(note.color),
             borderRadius: BorderRadius.circular(16),
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               ListTile(
-                title: const Text(
-                  'Flutter Tips',
+                title: Text(
+                  note.title,
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 26,
@@ -46,7 +47,7 @@ class NoteItem extends StatelessWidget {
                     bottom: 16,
                   ),
                   child: Text(
-                    'Build your Career with Khaled',
+                    note.subtitle,
                     style: TextStyle(
                       color: Colors.black.withOpacity(0.5),
                       fontSize: 18,
@@ -67,7 +68,7 @@ class NoteItem extends StatelessWidget {
                   right: 16,
                 ),
                 child: Text(
-                  'May 21 , 2022',
+                  note.date,
                   style: TextStyle(
                     color: Colors.black.withOpacity(0.4),
                     fontSize: 16,
